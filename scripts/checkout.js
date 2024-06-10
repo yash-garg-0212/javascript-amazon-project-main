36950e5abcae2f8beb67eee0e6b95b18d658c5e0
Yash Garg <yash.garg0212@gmail.com>
2024-06-10 14:27:19 +0530

    delete products from html when delete is pressed

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,8 @@ cart.forEach((cartItem)=>{
     }
   });
 
-  cartSummaryHTML +=`<div class="cart-item-container">
+  cartSummaryHTML +=`<div class="cart-item-container 
+  js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -95,5 +96,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click', () =>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 })
